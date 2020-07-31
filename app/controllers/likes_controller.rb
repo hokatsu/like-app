938bfe4585d11eac73bde4aff58b1816ddff8a75
hokatsu <46119001+hokatsu@users.noreply.github.com>
2020-07-31 13:29:50 +0900

    find_tweetメソッドを定義

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,20 @@
 class LikesController < ApplicationController
+  before_action :find_tweet
+
   def create
     like = current_user.likes.build(tweet_id: params[:tweet_id])
     like.save
-    @tweet = Tweet.find(params[:tweet_id])
     render 'create.js.erb'
   end
 
   def destroy
     like = Like.find_by(tweet_id: params[:tweet_id], user_id: current_user.id)
     like.destroy
-    @tweet = Tweet.find(params[:tweet_id])
     render 'destroy.js.erb'
   end
+  
+  def find_tweet
+    @tweet = Tweet.find(params[:tweet_id])
+  end
+
 end
